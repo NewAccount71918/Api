f687444b191e6cdb84e06926e96590d55b1d44a0
NewAccount71918 <vuga0471@gmail.com>
2025-03-13 20:35:36 +0700

    Update FullMon.js

--- api/FullMon.js
+++ api/FullMon.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
     res.setHeader("Content-Type", "text/plain");
-    res.send("setclipboard("https://discord.com/invite/hcJ8PHtkfy")");
+    res.send('setclipboard("https://discord.com/invite/hcJ8PHtkfy")');
 }
